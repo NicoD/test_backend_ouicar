62fa10d06e0a969e607557e7342de636ad4d7124
NicoD <n.denoual@gmail.com>
2019-07-07 18:13:26 +0200

    Car API resource

--- bin/install.sh
+++ bin/install.sh
@@ -4,3 +4,5 @@ docker-compose run php bin/console doctrine:database:create
 docker-compose run php bin/console doc:mig:mig
 docker-compose run php bin/console server:run
 
+
+# http://127.0.0.1:8000/api/
